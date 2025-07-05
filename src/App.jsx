943ce8269c6c2f96d3ab55d32ec3c9eae65c6811
Sethu Pyotr <sethupyotr@Sethus-MacBook-Pro.local>
2025-07-05 19:59:42 +0530

    lint check 3 logo hidden

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
           {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
         </a>
       </div>
       <h1>CI/CD + Vercel + Vite + React</h1>
